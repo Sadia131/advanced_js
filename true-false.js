58d97523e3280ae802568949e1e03520d6f7bc92
Sadia131 <sadiaobaed6969@gmail.com>
2022-08-26 12:35:47 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,13 @@
+//const name = "Salman";
+// 0,empty string,false, undefined hle, null,NaN are falsy
+
+//'0', ' ' => vitore space thakle, baki sb are truthy 
+let name = null;
+console.log(name);
+if(name){
+    console.log("Condition is true");
+}
+
+else{
+    console.log("Condition is false");
+}
